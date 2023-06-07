c834216e0620557377ee75d7a452aec6e5b9cc6f
IAmBill030202 <nhantvde160428@fpt.edu.vn>
2023-06-07 14:12:55 +0700

    PRM392 slot 9

---
+++ Slot_9/app/src/main/java/com/example/sqlite/ViewCourse.java
@@ -0,0 +1,32 @@
+package com.example.sqlite;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class ViewCourse extends AppCompatActivity {
+    private ArrayList<CourseModal> courseModalArrayList;
+    private DBHandler dbHandler;
+    private CourseRVAdapter courseRVAdapter;
+    private RecyclerView coursesRV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_courses);
+        //Initializing our all variables.
+        courseModalArrayList = new ArrayList<>();
+        dbHandler = new DBHandler(ViewCourse.this);
+        //Getting our course array list from db handler class
+        courseModalArrayList = dbHandler.readCourses();
+        courseRVAdapter = new CourseRVAdapter(courseModalArrayList, ViewCourse.this);
+        //Creating recycler view
+        coursesRV = findViewById(R.id.idRVCourses);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(ViewCourse.this, RecyclerView.VERTICAL,
+                false);
+        coursesRV.setLayoutManager(linearLayoutManager);
+        coursesRV.setAdapter(courseRVAdapter);
+    }
+}
